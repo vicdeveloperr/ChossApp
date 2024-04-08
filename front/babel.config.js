f9a2be371e5c4ff165d3d7f30936ad88d52a7f13
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-04-08 19:56:52 +0000

    Crea bable.config.js

---
+++ front/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo", "@babel/preset-typescript"],
+    plugins: ["react-native-reanimated/plugin"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
+  };
+};
