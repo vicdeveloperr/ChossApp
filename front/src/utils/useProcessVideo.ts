9a86b1e0bb6d91f89ee9175dc443e1155ea83828
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-04-18 19:01:34 +0000

    Devuelve useProcessVideo función para usarse

--- front/src/utils/useProcessVideo.ts
+++ front/src/utils/useProcessVideo.ts
@@ -2,22 +2,20 @@ import getVideoAnalysis from "./getVideoAnalysis";
 import { useRecordedStore, useSpeechStore } from "../stateManagement";
 import type { movementsAvailable } from "../types/movementsAvailable";
 
-type processVideo = (
+type processVideo = () => (
   uri: string,
   navigate: (route: string) => void,
   movementToImprove: movementsAvailable
 ) => Promise<void>;
 
-export const useProcessVideo: processVideo = async (
-  uri,
-  navigate,
-  movementToImprove
-) => {
+export const useProcessVideo: processVideo = () => {
   const { setRecorded } = useRecordedStore((state) => state);
   const { setSpeech } = useSpeechStore();
 
-  setRecorded(uri);
-  const path = await getVideoAnalysis(uri, movementToImprove);
-  setSpeech(path);
-  navigate("analysis");
+  return async (uri, navigate, movementToImprove) => {
+    setRecorded(uri);
+    const path = await getVideoAnalysis(uri, movementToImprove);
+    setSpeech(path);
+    navigate("analysis");
+  };
 };
