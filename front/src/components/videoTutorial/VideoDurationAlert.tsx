9aff13c230f0b7d4a59c7183a3d6c89c89245276
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-04-19 18:07:59 +0000

    Programa funcionalidad de VideoDurationAlert

--- front/src/components/videoTutorial/VideoDurationAlert.tsx
+++ front/src/components/videoTutorial/VideoDurationAlert.tsx
@@ -1,13 +1,23 @@
+import { useVideoPickerErrorDialogVisibilityStore } from "../../stateManagement";
 import AlertDialog from "../AlertDialog";
 import AlertDialogButton from "../AlertDialogButton";
 
-const VideoDurationAlert: React.FC = () => {
+export const VideoDurationAlert: React.FC = () => {
+  const { setVideoPickerErrorDialogVisibility } =
+    useVideoPickerErrorDialogVisibilityStore();
+
   return (
     <AlertDialog
       title="Vídeo demasiado largo"
       description="El vídeo es demasiado largo para ser procesado. Seleccione un vídeo de menos de 10 segundos"
-    >
-      <AlertDialogButton text="Continuar" />
-    </AlertDialog>
+      DialogActions={
+        <AlertDialogButton
+          action={() => {
+            setVideoPickerErrorDialogVisibility(false);
+          }}
+          text="Continuar"
+        />
+      }
+    ></AlertDialog>
   );
 };
