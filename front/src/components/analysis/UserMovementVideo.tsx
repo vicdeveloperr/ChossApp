80bbdae19a569dcc777784c7cf6e34179f294f76
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-04-20 13:52:58 +0000

    Soluciona errores de linter (UserMovementVideo)

--- front/src/components/analysis/UserMovementVideo.tsx
+++ front/src/components/analysis/UserMovementVideo.tsx
@@ -1,19 +1,23 @@
+import React from "react";
 import { useRecordedStore } from "../../stateManagement";
 import useLoaderVisibilityStore from "../../stateManagement/useLoaderVisibilityStore";
-import ScreenDarkModal from "../ScreenDarkModal";
 import { VideoFullScreen } from "../videoTutorial/VideoFullScreen";
 import { View } from "react-native";
 
-export const UserMovementVideo = () => {
+export const UserMovementVideo: React.FC = () => {
   const { setIsLoading } = useLoaderVisibilityStore((state) => state);
   const { recorded } = useRecordedStore((state) => state);
 
   return (
     <>
       <VideoFullScreen
-        onLoadInit={() => setIsLoading(true)}
+        onLoadInit={() => {
+          setIsLoading(true);
+        }}
         sourceUri={recorded}
-        onLoadComplete={() => setIsLoading(false)}
+        onLoadComplete={() => {
+          setIsLoading(false);
+        }}
       />
       <View testID="UserMovementVideo"></View>
     </>
