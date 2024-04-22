7b1d867376a0ad28f7a95326d58873718e325420
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-04-22 19:46:12 +0000

    Agrega parámetro a prcessVideo en su hook

--- front/src/utils/useProcessVideo.ts
+++ front/src/utils/useProcessVideo.ts
@@ -5,17 +5,21 @@ import type { movementsAvailable } from "../types/movementsAvailable";
 type processVideo = () => (
   uri: string,
   navigate: (route: string) => void,
-  movementToImprove: movementsAvailable
+  movementToImprove: movementsAvailable,
+  onGetVideoAnalysis?: () => void
 ) => Promise<void>;
 
 export const useProcessVideo: processVideo = () => {
   const { setRecorded } = useRecordedStore((state) => state);
   const { setSpeech } = useSpeechStore();
 
-  return async (uri, navigate, movementToImprove) => {
+  return async (uri, navigate, movementToImprove, onGetVideoAnalysis) => {
     setRecorded(uri);
+    navigate("analysis");
     const path = await getVideoAnalysis(uri, movementToImprove);
     setSpeech(path);
-    navigate("analysis");
+    if (onGetVideoAnalysis !== undefined) {
+      onGetVideoAnalysis();
+    }
   };
 };
